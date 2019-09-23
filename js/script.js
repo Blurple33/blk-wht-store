bd5e44332e4b3c0876b3f822ec730052926ef873
Blurple33 <leoncio68@gmail.com>
2019-09-23 15:57:53 -0700

    first commit; blank template

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// scroll to class [use as needed; delete if not]
+
+// var $ = jQuery;
+
+// $(function() {
+//   $('a[href*="#"]:not([href="#"]):not([href="#carousel-example-generic"])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         }, 600);
+//         return false;
+//       }
+//     }
+//   });
+// });
